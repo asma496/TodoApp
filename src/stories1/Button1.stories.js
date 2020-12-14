edbd9af1b1bce5464a0131f5a0b8b62e3d171ee2
asma496 <asmaashraf496@gmail.com>
2020-12-14 12:55:52 +0500

    'done'

---
+++ src/stories1/Button1.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '../components/Button'
+
+export default{
+    title:'Button',
+    component: Button
+
+}
+export const btn = () => <Button text='Add' />
+export const btn1 = () => <Button text='Addtodo' />
+export const btn2 = () => <Button text='Add in list' />
